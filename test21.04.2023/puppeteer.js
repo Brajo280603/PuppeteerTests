04b827131580bb765772143a080b45ff0fa3b9b4
Brajananda Mahata <brajanandamahato280603@gmail.com>
2023-04-21 16:29:49 +0530

    test rerun

--- test21.04.2023/puppeteer.js
+++ test21.04.2023/puppeteer.js
@@ -5,9 +5,9 @@ function puppeteerapi(URLans){
   async function getVisual() {
     try{
       const URL = URLans;
-      // const browser = await puppeteer.launch(); //launch headless by default
+      const browser = await puppeteer.launch(); //launch headless by default
 
-      const browser = await puppeteer.launch({headless:false});//this will launch it with a gui
+      // const browser = await puppeteer.launch({headless:false});//this will launch it with a gui
 
 
         
@@ -16,21 +16,21 @@ function puppeteerapi(URLans){
       await page.setViewport({ width: 1366, height: 768});
       await page.goto(URL)
 
-      // await page.screenshot({path: 'sclocal.png'})
+      await page.screenshot({path: 'sclocal.png'})
       
-      // await page.pdf({path:'pdflocal.pdf'})
+      await page.pdf({path:'pdflocal.pdf'})
 
       // Query for an element handle.
-      const element = await page.waitForSelector('div > .getHere');
+    //  const element = await page.waitForSelector('div > .getHere');
 
       // Do something with element...
-      await element.click(); // Just an example.
+    //  await element.click(); // Just an example.
 
       // Dispose of handle
-      await element.dispose();
+    //  await element.dispose();
 
 
-      // await browser.close()
+      await browser.close()
     } catch(error) {
       console.error(error)
     }
@@ -41,4 +41,4 @@ function puppeteerapi(URLans){
 
 }
 
-puppeteerapi("http://localhost:1234/")
+puppeteerapi("https:brajo280603.github.io/FreeEpicGamesApp/")
